17f5020ab08a0a781b245242a68a1416cd759b99
mouss3abbb <mouss3abbb@gmail.com>
2023-03-02 18:43:16 +0000

    Created screens files

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:refugees_help/screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const MaterialApp(
+        home: SplashScreen(),
+    );
   }
 }
